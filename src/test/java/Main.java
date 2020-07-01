359f37763d904df88f341e2b7aa6b536201e3eed
mrjimmyjimmy <mrxujimmy@gmail.com>
2020-07-01 16:11:16 +0800

    update

---
+++ src/test/java/Main.java
@@ -0,0 +1,15 @@
+/**
+ * @author jimmy xu
+ * @date 2020/6/24 14:02
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        int i = 0;
+        i = i++;
+        System.out.println(i);
+
+    }
+
+
+}
