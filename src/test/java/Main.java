4166e021db25706dfb9168c71dd325eb8e925a0d
mrjimmyjimmy <mrxujimmy@gmail.com>
2020-07-29 08:16:59 +0800

    add first C program

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -3,13 +3,26 @@
  * @date 2020/6/24 14:02
  */
 public class Main {
-
+    public static int a;
     public static void main(String[] args) {
-        int i = 0;
-        i = i++;
-        System.out.println(i);
-
+        int x = 1;
+        Main.a = 1;
     }
 
+    public static double max(int a, int b) {
+        for (int i = 0; i < 10; i++) {
+            Main.a = a;
+        }
+        for (int i = 0; i < 10; i++) {
+
+        }
+        if (a > b) return a;
+        else return b;
+    }
+
+    public static double max(double a, double b) {
+        if (a > b) return a;
+        else return b;
+    }
 
 }
