ba211c10adabc806d9de02f171aae902245c6482
mrjimmyjimmy <mrxujimmy@gmail.com>
2020-09-02 17:24:05 +0800

    ch9 tv

---
+++ src/main/java/ch9/Tv.java
@@ -0,0 +1,50 @@
+package ch9;
+
+/**
+ * @author jimmy xu
+ * @date 2020/9/2 17:15
+ */
+public class Tv {
+
+    private int channel;
+    private int volumeLevel;
+    private boolean on;
+
+    public Tv() {
+        channel = 1;
+        volumeLevel = 1;
+        on = false;
+    }
+
+    public void turnOn() {
+        on = true;
+    }
+
+    public void turnOff() {
+        on = false;
+    }
+
+    public void setChannel(int channel) {
+        this.channel = channel;
+    }
+
+    public void setVolume(int volume) {
+        volumeLevel = volume;
+    }
+
+    public void channelUp() {
+        channel++;
+    }
+
+    public void channelDown() {
+        channel--;
+    }
+
+    public void volumeUp() {
+        volumeLevel++;
+    }
+
+    public void volumeDown() {
+        volumeLevel--;
+    }
+}
